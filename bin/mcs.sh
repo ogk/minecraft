73ffc8b0422c3ce51dcb81351f9a855a3556e744
Olav Georg Karlsen <olav@ogk.no>
2014-12-29 23:04:06 +0100

    Tilsynelatende starter faktisk minecraft. Lagt til Create og Log

--- bin/mcs.sh
+++ bin/mcs.sh
@@ -1,33 +1,83 @@
 #!/bin/bash
 
+#===========================================================
+# Deklarasjon av variable + evt. debug
+#===========================================================
+
+[[ -n "$DEBUG" ]] && set -x # turn -x on if DEBUG is set to a non-empty string
+[[ -n "$NOEXEC" ]] && set -n # turn -n on if NOEXEC is set to a non-empty string
+#set -o nounset # Feiler hvis man prøver å bruke en uinitialisert variabel
+#set -o errexit # Avslutter umiddelbart hvis et statement returnerer false
+
+
+#===========================================================
+# Deklarasjon av funksjoner
+#===========================================================
+
+
+Create() {
+    local name=$1
+    [[ -z "$name" ]] && Fail "Navn på verden må angis"
+    mkdir -p ~/minecraft/
+    cp -rp ~/minecraft/skel ~/minecraft/worlds/$name || Fail "Kopiering av skel til verden $name feilet!"
+}
+
 Fail() {
     echo "$1"
     exit 1
 }
 
+Log() {
+    local name=$1
+    [[ -z "$name" ]] && Fail "Navn på verden må angis"
+    less +G ~/minecraft/worlds/$name/logs/server.log || Fail "Fant ikke loggfil under $name"
+}
+
+
+Status() {
+    local pid=$(pgrep -f '^java.*minecraft.jar')
+    if [[ -n "$pid" ]]; then
+        echo "Minecraft server er oppe på pid $pid."
+        exit 0
+    else
+        echo "Ingen Minecraft server funnet."
+        exit 1
+    fi
+}
+
 Start() {
-    cd ~/minecraft/current || Fail "Kunne ikke bytte katalog"
-    nohup java -Xmx512M -Xms128M -jar ~/minecraft/current/minecraft_server.jar nogui &
+    local name=$1
+    [[ -z "$name" ]] && Fail "Navn på verden må angis"
+    cd ~/minecraft/worlds/$name || Fail "Kunne ikke bytte til katalog ~/minecraft/worlds/$name"
+    nohup java -Xmx1G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -jar ./minecraft.jar nogui >> logs/server.log 2>&1 &
 }
 
 Stop() {
-    echo "Killing process..."
+    echo "Killing $(pgrep -f '^java.*minecraft.jar')"
     pkill -uminecraft -f "^java.*minecraft_server.jar"
 }
 
 Usage() {
     echo "Usage:"
-     echo "$(basename $0) <start|stop>"
+     echo "$(basename $0) <create|status|start|stop>"
 }
 
-case "$1" in
+[[ ! -d ~/minecraft/skel ]] && Fail "Dette git-repoet må klones under din hjemmekatalog, dvs. navngis '~/minecraft'. Avslutter."
+
+CMD=$1
+shift
+
+case "$CMD" in
+    create)
+        Create $* ;;
+    log)
+        Log $* ;;
     start)
-        Start;;
+        Start $* ;;
+    status)
+        Status ;;
     stop)
-        Stop;;
+        Stop ;;
     *)
-        Usage;;
+        Usage ;;
 esac
-
-
-

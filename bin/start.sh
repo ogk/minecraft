ede9da9b33e875d52381b620681b1b5f72156b23
Olav G. Karlsen <olav@ogk.no>
2016-01-30 20:09:05 +0100

    Fjerner unødvendige scripts

--- bin/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -Xmx1G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -jar ./minecraft.jar nogui >> logs/server.log 2>&1 &
-
--- bin/stop.sh
+++
@@ -1,3 +0,0 @@
-echo "Killing $(pgrep -f '^java.*minecraft.jar')"
-pkill -f '^java.*minecraft.jar'
-
